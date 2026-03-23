bb118b4ebb3fa83b4e768c386be18b931efc9d96
Kyle Nguyen <kyleqbnguyen@gmail.com>
2026-03-22 17:15:07 -0700

    bubble sort

--- src/dsa/bubble_sort.h
+++ src/dsa/bubble_sort.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#include <cstddef>
 #include <vector>
 
 namespace dsa {
 
 // Sort data in place in ascending order.
 inline void bubble_sort(std::vector<int> &data) {
-  // TODO
-  (void)data;
+  for (std::size_t i {}; i < data.size(); i++) {
+    for (std::size_t j{}; j < data.size() - 1 - i; j++) {
+      int tmp {data[j]};
+
+      if (data[j] > data[j + 1]) {
+        data[j] = data[j + 1];
+        data[j + 1] = tmp;
+      }
+    }
+  }
 }
 
 } // namespace dsa
